9aa8933292c7f59ecb2208235cc7eebce3f5fabe
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-09-22 21:22:04 -0300

    projeto front end squid

--- scripts.js
+++ scripts.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Código que será executado quando o navegador carregar
+  fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic')
+       .then((response) => {
+        response.json().then((result) => {
+          createImage(result);
+        });
+       });
 });
+
+function createImage(result) {
+  const section = document.createElement('section');
+  document.querySelector('.main_site').appendChild(section);
+  section.className ='container';
+  result.forEach((img) => {
+    section.appendChild(createImageElement(img));
+
+
+  })
+}
+  function createImageElement(obj) {
+    const img = document.createElement('img');
+    img.className = 'img_instagram';
+    img.src = obj.imagens.thumbnail.url;
+    img.addEventListener('click', () => {
+      console.log(obj.imagens);
+      window.open(obj.link);
+});
+    return img;
+  }
+
